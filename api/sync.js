06ca86da42398aaf0dd42173833d11f4e5f025ff
Frankz <frank.alvarz94@gmail.com>
2026-05-21 20:29:36 -0600

    DELETE FUNCTION COMMENTED

--- api/sync.js
+++ api/sync.js
@@ -371,7 +371,7 @@ async function syncProperties(page = 1, webflowMap) {
   // ─── DELETE MISSING ─────────────────────────
 
   // SOLO en última página
-  if (page === totalPages) {
+  /*if (page === totalPages) {
     for (const [key, item] of webflowMap.entries()) {
       if (!domusKeys.has(key)) {
         await webflowRequest(
@@ -394,7 +394,7 @@ async function syncProperties(page = 1, webflowMap) {
     created,
     updated,
     deleted
-  };
+  };*/
 }
 
 // ─── HANDLER ─────────────────────────────────────────────────────────────────
